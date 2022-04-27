1b0ec69dced1b972d7ca240dc8c2237861040b6d
Gabrielle Walsh <gabmwalsh@gmail.com>
2022-04-27 09:25:48 -0700

    added readme and other changes

--- script.js
+++ script.js
@@ -2,35 +2,24 @@
 
 // - When the user clicks on the gray button, make the background gray and font white
 
-$("#grayButton").click(function(){
-    $("body").css("background", "gray");
+$("#darkButton").click(function(){
+    $("body").css(background-image url("assets/noita-digital-QZX8jbK1xEw-unsplash\ \(1\).jpg"));
     $("body").css("color", "white");
   });
 
 
 // - When the user clicks on the white button, make the background white and font black
-$("#whiteButton").click(function(){
+$("#lightButton").click(function(){
     $("body").css("background", "white")
     $("body").css("color", "black"); 
 });
-
-$(".socials li a i").on("mouseover", (event) => {
-    $(event.target).addClass("fa-bounce");
-});
-
-$(".socials li a i").on("mouseout", (event) => {
-    $(event.target).removeClass("fa-bounce");
-});
-
-// html
-
-// <ul id="switcher">
-// <li id="grayButton"></li>
-// <li id="whiteButton"></li>
-// </ul>
-
-
-
+//Social Functions for Desktop
+// $(".socials li a i").on("mouseover", (event) => {
+//     $(event.target).addClass("fa-bounce");
+// });
+// $(".socials li a i").on("mouseout", (event) => {
+//     $(event.target).removeClass("fa-bounce");
+// });
 
 // css
 
@@ -48,26 +37,4 @@ $(".socials li a i").on("mouseout", (event) => {
 //   }
 //   p {
 //     margin: .5em 0;
-//   }
-  
-//   #switcher {
-//     list-style: none;
-//     margin: 0;
-//     padding: 0;
-//     overflow: hidden;
-//   }
-//   #switcher li {
-//     float: left;
-//     width: 30px;
-//     height: 30px;
-//     margin: 0 15px 15px 0;
-//     border-radius: 30px;
-//     border: 3px solid black;
-//   }
-  
-//   #grayButton {
-//     background: gray;
-//   }
-//   #whiteButton {
-//     background: white;
 //   }
